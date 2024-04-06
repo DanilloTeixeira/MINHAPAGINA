8e09780aa7382194cc731877ed32bb18182ab8ba
Danillo Cesar <danillo81@hotmail.com>
2024-04-06 14:00:40 -0300

    Update

---
+++ script.js
@@ -0,0 +1,31 @@
+JavaScript
+// Menu Fixo
+window.addEventListener('scroll', function () {
+  const nav = document.querySelector('nav');
+  if (window.scrollY > 200) {
+    nav.classList.add('fixed');
+  } else {
+    nav.classList.remove('fixed');
+  }
+});
+
+// Galeria de Imagens (Exemplo Simples)
+const imagens = document.querySelectorAll('#galeria img');
+const galeriaAtual = document.querySelector('#galeria-atual');
+const indicadorAtual = document.querySelector('#indicador-atual');
+
+let imagemAtual = 0;
+
+imagens.forEach((imagem, index) => {
+  imagem.addEventListener('click', () => {
+    mostrarImagem(index);
+  });
+});
+
+function mostrarImagem(index) {
+  imagemAtual = index;
+  galeriaAtual.src = imagens[imagemAtual].src;
+  indicadorAtual.textContent = (imagemAtual + 1) + ' / ' + imagens.length;
+}
+
+// Adicione mais funcionalidades JavaScript conforme suas necessidades
